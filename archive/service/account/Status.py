5bbc9504857c24a3aeea09010548e0df725c0f18
Ritsuki Otake <rtk.otk@gmail.com>
2023-08-20 23:43:24 +0900

    関係ないファイルの削除

--- archive/service/account/Status.py
+++
@@ -1,9 +0,0 @@
-from letusc.Letus.v2.LetusAccount import LetusAccount
-from letusc.logger import Log
-
-
-def Status(LA: LetusAccount):
-    __logger = Log("Service.Account.Status")
-    # TODO: check status account
-    # try:
-    #     statusAccount
--- src/letus_scraper/src/service/v2/middleware/getAccount.py
+++
@@ -1,5 +0,0 @@
-from src.Letus.v2.LetusPage import LetusPage
-
-
-def getAccount(LP: LetusPage):
-    return LP.LA
