ab83b20a0f64a6c9bec456b70e679b932bef9836
Ritsuki Otake <rtk.otk@gmail.com>
2023-08-31 15:37:43 +0900

    各テストの追加

--- letusc/test/__init__.py
+++ letusc/test/__init__.py
@@ -1,4 +1,5 @@
 from letusc.logger import Log
+from letusc.MessageBuilder import MessageBuilder
 
 
 def test():
@@ -7,15 +8,17 @@ def test():
     # test_content()
     # test_module()
     # test_parser()
-    test_content_task()
-    exit(0)  # debug: exit here
+    # test_content_task()
+    # test_diff_texts()
+    # test_discord_webhook()
+    # exit(0)  # debug: exit here
     pass
 
 
 def test_account():
     __logger = Log("test.test_account")
     __logger.info("test_account")
-    from letusc.Model.Account import Account
+    from letusc.Model.account import Account
 
     account = Account("7322023")
     __logger.debug(account.__dict__)
@@ -25,7 +28,7 @@ def test_account():
 def test_pages():
     __logger = Log("test.test_pages")
     __logger.info("test_pages")
-    from letusc.Model.Page import Page
+    from letusc.Model.page import Page
 
     page = Page.from_code("2023:course:126936")
     __logger.debug(page.__dict__)
@@ -36,7 +39,7 @@ def test_pages():
 def test_content():
     __logger = Log("test.test_content")
     __logger.info("test_content")
-    from letusc.Model.Content import Content
+    from letusc.Model.content import Content
 
     content1 = Content.from_code("2023:course:126936:section:1078135")
     __logger.debug(content1.__dict__)
@@ -53,7 +56,7 @@ def test_content():
 def test_module():
     __logger = Log("test.test_module")
     __logger.info("test_module")
-    from letusc.Model.Module import Module
+    from letusc.Model.module import Module
 
     module1 = Module.from_code("2023:course:126936:section:1081922:label:1277831")
     __logger.debug(module1.__dict__)
@@ -70,8 +73,8 @@ def test_module():
 def test_parser():
     __logger = Log("test.test_parser")
     __logger.info("test_parser")
+    from letusc.Model.account import Account
     from letusc.PageParser import PageParser
-    from letusc.Model.Account import Account
 
     account = Account("7322023")
     parser = PageParser(account, "2023:course:126936")
@@ -95,3 +98,47 @@ def test_content_task():
     )
     task.run()
     pass
+
+
+def test_diff_texts():
+    __logger = Log("test.test_diff_texts")
+    __logger.info("test_diff_texts")
+    from letusc.util.diff import diff_texts
+
+    text1 = "EEの学生向け掲示板です（大学からの情報はCLASSで行われます）。"
+    text2 = "EEの学生向け掲示板です（大学からの情報はCLASSで行われますので、CLASSも必ずご覧ください）。\n学科内の情報ですので、SNSなどでの拡散は禁止します。"
+    diff = diff_texts(text1, text2)
+    for line in diff:
+        # __logger.debug(line)
+        print(line)
+
+
+def test_discord_webhook():
+    __logger = Log("test.test_discord_webhook")
+    __logger.info("test_discord_webhook")
+    from datetime import datetime
+
+    THREAD_ID = 1145207293105942668
+    builder = MessageBuilder(thread_id=str(THREAD_ID))
+    builder.addEmbed(
+        title="「EE 共通掲示板」における変更通知 `course:126936`",
+        description="section:1081922",
+        url="https://letus.ed.tus.ac.jp/course/view.php?id=126936",
+        timestamp=datetime.now(),
+        color=0x00FFFF,
+    )
+    builder.addEmbed(
+        title="PC・IT環境設定について",
+        description="no change",
+        url="https://letus.ed.tus.ac.jp/mod/page/view.php?id=828743",
+        timestamp=datetime.now(),
+        color=0x00FFFF,
+    )
+    builder.addEmbed(
+        title="「学園生活」の電子化について",
+        description="これまで冊子で配布していました。「学園生活」について、電子版に変わりました。\n\r\n証明証や奨学金などの手続方法や、生活サポートなど、常に最新の情報が公開されます。\n\r\n新入生は必ずご一読ください。",
+        url="https://letus.ed.tus.ac.jp/mod/url/view.php?id=785895",
+        timestamp=datetime.now(),
+        color=0x00FFFF,
+    )
+    builder.send()
