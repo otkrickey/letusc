3b6884500bbd7c7321d12bc10c8828c624a64a41
Ritsuki Otake <rtk.otk@gmail.com>
2023-08-31 15:37:34 +0900

    ContentTaskを中心にタスクを実装

--- letusc/Task/Content.py
+++ letusc/Task/Content.py
@@ -1,7 +1,10 @@
 from dataclasses import dataclass
 
 from letusc.logger import Log
-from letusc.Model.Account import Account
+from letusc.MessageBuilder import MessageBuilder
+from letusc.Model.account import Account
+from letusc.Model.content import Content
+from letusc.Model.module import Module
 from letusc.PageParser import PageParser
 
 from .BaseTask import BaseTask
@@ -55,8 +58,69 @@ class FetchContent(ContentTask):
     def run(self):
         __logger = Log("Task.ContentTask.FetchContent.run")
         self.parser.parse()
-        results = self.parser.compare()
-        for result in results:
-            __logger.debug(
-                f"status: {result['status']}, type: {result['type']}, object_id: {repr(result['object'])}"
+        contents = self.parser.compare()
+        if len(contents) == 0:
+            return
+
+        page_title = self.parser.page.title
+
+        ignore_ids = ["1081922", "1078139"]
+
+        for content in contents:
+            code = content["code"]
+            status = content["status"]
+            type = content["type"]
+            new = content["new"]
+            old = content["old"]
+            modules = content["modules"]
+            if not isinstance(new, Content) or not isinstance(old, Content):
+                continue
+
+            content = f"{page_title}「{new.title}」(`{code}`)"
+            if status == "new":
+                content += "が追加されました。"
+            elif status == "changed":
+                content += "に変更がありました。"
+            builder = MessageBuilder(
+                content=content,
+                thread_id="1146305009076686868",
             )
+            __logger.debug("-" * 70)
+            short_code = f"`{new.content_type}:{new.content_id}`"
+            __logger.debug(f"{status}, {short_code}, 「{new.title}」")
+
+            fields = []
+
+            for module in modules:
+                code = module["code"]
+                status = module["status"]
+                type = module["type"]
+                new = module["new"]
+                old = module["old"]
+                if not isinstance(new, Module) or not isinstance(old, Module):
+                    continue
+                __logger.debug("-" * 90)
+                name = f"[{new.module_type}] {new.title} ({status}!)"
+                short_code = f"`{new.module_type}:{new.module_id}`"
+                value = f"{new.main or 'no description'}\n[{short_code}]({new.module_url or new.url})\n"
+                fields.append({"name": name, "value": value, "inline": False})
+
+                __logger.debug(f"{status}, {short_code}, 「{new.title}」")
+
+                for ignore_id in ignore_ids:
+                    if ignore_id not in new.code:
+                        new.push()
+
+            for ignore_id in ignore_ids:
+                if ignore_id not in new.code:
+                    new.push()
+
+            builder.addEmbed(
+                title=f"{page_title}: 「{new.title}」 ({status}!)",
+                description=new.main or "no description",
+                url=f"{new.url}&title={new.title}",
+                timestamp=new.timestamp,
+                color=0x56D364 if status == "new" else 0xE3B341,
+                fields=fields,
+            )
+            builder.send()
